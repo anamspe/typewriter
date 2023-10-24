497a9f836d4720203f220548c3337b825e601702
Ana <anmeuri@gmail.com>
2023-10-24 14:59:21 -0700

    First commit

--- typewriter.js
+++ typewriter.js
@@ -6,6 +6,6 @@ for (const char of sentence) {
     //print the char here
     process.stdout.write(char);
 
-  }, 1000) // 1s delay to make it noticeable. Can dial it down later.
+  }, 1000) // 1s delay to make it noticeable. Can dial it down later...
 
 }
